782286aa467480cfabf4f6e1027a0e45ced79805
Shubham Kumar <iamkumarshubham17@gmail.com>
2025-11-21 13:02:47 +0530

    Initial backend commit

---
+++ models/userModel.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    role: {
+      type: String,
+      required: [true, "role is required"],
+      enum: ["admin", "organisation", "donar", "hospital"],
+    },
+
+    name: {
+      type: String,
+      required: function () {
+        return this.role === "donar" || this.role === "admin";
+      },
+    },
+
+    organisationName: {
+      type: String,
+      required: function () {
+        return this.role === "organisation";
+      },
+    },
+
+    hospitalName: {
+      type: String,
+      required: function () {
+        return this.role === "hospital";
+      },
+    },
+
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "password is required"], 
+    },
+
+    website: {
+      type: String,
+    },
+
+    address: {
+      type: String,
+      required: [true, "address is required"],
+    },
+
+    phone: {
+      type: String,
+      required: [true, "phone number is required"], 
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("users", userSchema);
